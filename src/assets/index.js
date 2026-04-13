7a4f13d1d0001606c5533746a66441d63e8f93bf
ifeobi <ifesuper13@gmail.com>
2026-04-13 22:19:14 +0100

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,65 @@
+import download from "./download.png";
+import logo from "./logo.svg";
+import logo2 from "./logo2.png";
+import preview from "./preview.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import mobile from "./mobile.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import web from "./web.png";
+import javascript from "./tech/javascript.png";
+import typescript from "./tech/typescript.png";
+import html from "./tech/html.png";
+import css from "./tech/css.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import nodejs from "./tech/nodejs.png";
+import mongodb from "./tech/mongodb.png";
+import git from "./tech/git.png";
+import figma from "./tech/figma.png";
+import docker from "./tech/docker.png";
+import onlyjewels from "./company/onlyjewels.png";
+import stripe from "./company/stripelogo.png";
+import mrparts from "./company/mrpartslogo.jpg";
+import shopify from "./company/shopify.png";
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import threejs from "./tech/threejs.svg";
+import github from "./github.png";
+
+export {
+  github,
+  download,
+  logo,
+  logo2,
+  preview,
+  menu,
+  close,
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  onlyjewels,
+  stripe,
+  mrparts,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+};
